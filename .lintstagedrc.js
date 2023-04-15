58627be42046c1bc1712964178c115ab98e30833
Ville Häkli <ville@vhx.fi>
2023-04-15 16:20:23 +0300

    Organize packages, improve lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"**/*.ts?(x)": (filenames) => {
+	"**/*.ts": (filenames) => {
 		const packages = filenames.reduce((acc, filename) => {
 			const match = filename.match(/packages\/([^/]+)/);
 			if (match && !acc.includes(match[0])) {
@@ -7,6 +7,8 @@ module.exports = {
 			}
 			return acc;
 		}, []);
-		return packages.map((pkg) => `tsc -p ./${pkg}/tsconfig.json`);
+		const tsc = packages.map((pkg) => `tsc -p ./${pkg}/tsconfig.json`);
+		return [...tsc, `eslint --fix ${filenames.join(" ")}`];
 	},
+	"*.{js,jsx,ts,tsx,css,md,json}": ["prettier --write"],
 };
